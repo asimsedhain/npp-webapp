6f3edb1c1795dff1f76d6248f305af5570a0b10d
Mallika Mainali <mmainali@patriots.uttyler.edu>
2020-10-01 20:48:45 -0500

    Created course component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Card, Button, Typography } from "@material-ui/core";
+import RemoveIcon from "@material-ui/icons/Remove";
+
 
 function Dashboard() {
 	return (
-		<>
-			<h1>hello</h1>
-		</>
+		<div classname="Dashboard">
+			<DraggableCard text="COSC 4336: SOFTWARE DEVELOPMENT" />
+			<DraggableCard text="COSC 4336: SOFTWARE DEVELOPMENT" />
+			<DraggableCard text="COSC 4336: SOFTWARE DEVELOPMENT" />
+		</div>
 	);
 }
+const DraggableCard = ({ text }) => {
+	return (
+
+		<Card
+			style={{ width: "22rem", backgroundColor: "#FEFAFA", color: "blue", height: "35px", margin: 10 }}
+		>
+			<Typography variant="subtitle">{text}
+				<Button onClick={()=>console.log("delete")}>
+  <RemoveIcon></RemoveIcon></Button>
+			</Typography>
+		</Card>
+
+
+	);
+};
 
 export default Dashboard;
