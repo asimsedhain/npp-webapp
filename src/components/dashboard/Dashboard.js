5deeb1cfc5764cf5b60804b2cf4b92aab0e2cbcf
unknown <balampakisakar@gmail.com>
2020-10-01 19:42:58 -0500

    Added container

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import Container from "@material-ui/core/Container";
+import "./Dashboard.css"
 function Dashboard() {
 	return (
-		<>
-			<h1>hello</h1>
-		</>
+		
+		<Container>
+			<h1> </h1>
+			<div class="header">
+  <h1>My Courses Plan</h1>
+  <p>Total Credit Hours: </p>
+</div>
+		<div
+      style={{
+        backgroundColor: '#C4C4C4',
+        width: '1232px',
+        height: '900px'
+      }}
+    />
+		</Container>
 	);
 }
 
