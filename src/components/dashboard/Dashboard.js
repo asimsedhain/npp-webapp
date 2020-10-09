43f1f1c93741a6ec8d8cb292956a0ebb9d85ce7d
Mallika Mainali <mmainali@patriots.uttyler.edu>
2020-10-08 20:53:18 -0500

    Deleted the unnecessary file and created a separate file for CardCourse components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,29 +1,14 @@
 import React from "react";
-import { Button, Typography } from "@material-ui/core";
-import RemoveIcon from "@material-ui/icons/Remove";
-import { CardBorder, StyledCard } from "./StyledDashboardComponents";
+import { CardCourse } from "./CardCourseComponents";
 
 function Dashboard() {
 	return (
 		<div>
-			<CardCourse text="COSC 4336: SOFTWARE DEVELOPMENT" onClickFunction={() => { console.log("Hello") }} bgColor="orange"/>
-			<CardCourse text="COSC 4336: SOFTWARE DEVELOPMENT" onClickFunction={() => { console.log("Bye") }} bgColor="green"/>
-			<CardCourse text="COSC 4336: SOFTWARE DEVELOPMENT" onClickFunction={() => { console.log("Hello again") }} bgColor="gray"/>
+			<CardCourse text="COSC 4336: SOFTWARE DEVELOPMENT" onClickFunction={() => { console.log("Hello") }} bgColor="orange" />
+			<CardCourse text="COSC 4336: SOFTWARE DEVELOPMENT" onClickFunction={() => { console.log("Bye") }} bgColor="green" />
+			<CardCourse text="COSC 4336: SOFTWARE DEVELOPMENT" onClickFunction={() => { console.log("Hello again") }} bgColor="gray" />
 		</div>
 	);
 }
-
-const CardCourse = ({ text, onClickFunction, bgColor }) => {
-	return (
-		<StyledCard>
-			<CardBorder bgColor={bgColor}>
-				<Typography variant="subtitle"> {text}
-					<Button onClick={() => onClickFunction()}>
-						<RemoveIcon> </RemoveIcon></Button>
-				</Typography>
-			</CardBorder>
-		</StyledCard>
-	);
-};
 
 export default Dashboard;
